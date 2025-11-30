db1474397cb1768e5b8c53dbb5ce6b9a4edf5ec6
Leendert de Borst <ldeborst@xivisoft.com>
2025-11-30 16:29:18 +0100

    Add cascade delete to MobileLoginRequests (#1415)

--- apps/server/Databases/AliasServerDb/Migrations/20251130152655_ConfigureMobileLoginRequestCascadeDelete.Designer.cs
+++ apps/server/Databases/AliasServerDb/Migrations/20251130152655_ConfigureMobileLoginRequestCascadeDelete.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AliasServerDb;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AliasServerDb.Migrations
 {
     [DbContext(typeof(AliasServerDbContext))]
-    partial class AliasServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251130152655_ConfigureMobileLoginRequestCascadeDelete")]
+    partial class ConfigureMobileLoginRequestCascadeDelete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
