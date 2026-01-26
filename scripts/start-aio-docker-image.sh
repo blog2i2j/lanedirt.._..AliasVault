e2fbadadf523e815e8c5e60955b68191e57520c5
Leendert de Borst <ldeborst@xivisoft.com>
2026-01-26 15:47:06 +0100

    Add aio-docker helper scripts for local testing (#1493)

---
+++ scripts/start-aio-docker-image.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Go to root
+cd ..
+
+# Build the all-in-one docker image
+echo "Building all-in-one docker image..."
+docker build \
+  -f dockerfiles/all-in-one/Dockerfile \
+  -t aliasvault-allinone:local \
+  .
+
+# Stop the all-in-one docker container if it is running
+docker stop aliasvault && docker rm aliasvault
+
+# Run the all-in-one docker container
+echo "Running all-in-one docker container..."
+docker run -d \
+  --name aliasvault \
+  -p 80:80 \
+  -v ./database:/database \
+  -v ./logs:/logs \
+  -v ./secrets:/secrets \
+  aliasvault-allinone:local
+
+  # Go back to scripts
+  cd scripts
---
+++ scripts/stop-aio-docker-image.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Stop the all-in-one docker container
+docker stop aliasvault
