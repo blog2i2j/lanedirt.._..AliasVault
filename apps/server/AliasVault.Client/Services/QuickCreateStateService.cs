cb90dc11999f7ec92a1db8fd1d541fd6be6f9ece
Leendert de Borst <ldeborst@xivisoft.com>
2026-01-27 22:21:07 +0100

    Fix AliasVault.Client issue when creating new item when already on the item AddEdit page

--- apps/server/AliasVault.Client/Services/QuickCreateStateService.cs
+++ apps/server/AliasVault.Client/Services/QuickCreateStateService.cs
@@ -13,6 +13,11 @@ namespace AliasVault.Client.Services;
 public class QuickCreateStateService
 {
     /// <summary>
+    /// Event that is raised when the state has been updated and the consumer should reinitialize.
+    /// </summary>
+    public event Action? OnChange;
+
+    /// <summary>
     /// Gets or sets the service name from quick create.
     /// </summary>
     public string? ServiceName { get; set; }
@@ -31,6 +36,11 @@ public class QuickCreateStateService
     /// Gets or sets the folder ID to pre-select when creating a new item.
     /// </summary>
     public Guid? FolderId { get; set; }
+
+    /// <summary>
+    /// Notifies subscribers that the state has changed.
+    /// </summary>
+    public void NotifyStateChanged() => OnChange?.Invoke();
 
     /// <summary>
     /// Clears the stored state.
