dff49379b5f1f29ed6acf2654440012c761a0855
Leendert de Borst <ldeborst@xivisoft.com>
2026-02-20 14:06:48 +0100

    Update UnlockSuccess.tsx button colors for light mode (#1756)

--- apps/browser-extension/src/entrypoints/popup/pages/auth/UnlockSuccess.tsx
+++ apps/browser-extension/src/entrypoints/popup/pages/auth/UnlockSuccess.tsx
@@ -154,7 +154,7 @@ const UnlockSuccess: React.FC = () => {
         </button>
         <button
           onClick={handleBrowseVaultContents}
-          className="w-full px-4 py-2 text-gray-700 bg-gray-100 rounded hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600"
+          className="w-full px-4 py-2 text-gray-900 bg-white border border-gray-300 rounded hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600 dark:border-gray-600"
         >
           {t('auth.browseVault')}
         </button>
