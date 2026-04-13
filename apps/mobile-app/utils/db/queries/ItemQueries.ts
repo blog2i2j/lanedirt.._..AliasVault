9fd8e032469a76cbe6f0ce978692fb871821e0ee
Leendert de Borst <ldeborst@xivisoft.com>
2026-04-13 11:30:26 +0200

    Update ItemQueries.ts (#1903)

--- apps/mobile-app/utils/db/queries/ItemQueries.ts
+++ apps/mobile-app/utils/db/queries/ItemQueries.ts
@@ -145,7 +145,7 @@ export class ItemQueries {
       l.FileData as Logo,
       CASE WHEN EXISTS (SELECT 1 FROM Passkeys pk WHERE pk.ItemId = i.Id AND pk.IsDeleted = 0) THEN 1 ELSE 0 END as HasPasskey,
       CASE WHEN EXISTS (SELECT 1 FROM Attachments att WHERE att.ItemId = i.Id AND att.IsDeleted = 0) THEN 1 ELSE 0 END as HasAttachment,
-      CASE WHEN EXISTS (SELECT 1 FROM TotpCodes tc WHERE tc.ItemId = i.Id AND pk.IsDeleted = 0) THEN 1 ELSE 0 END as HasTotp,
+      CASE WHEN EXISTS (SELECT 1 FROM TotpCodes tc WHERE tc.ItemId = i.Id AND tc.IsDeleted = 0) THEN 1 ELSE 0 END as HasTotp,
       i.CreatedAt,
       i.UpdatedAt,
       i.DeletedAt
